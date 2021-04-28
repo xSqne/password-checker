44adc620bfc4033fd76339c64c0d8beaf8d60632
xSqne <ileem0519@gmail.com>
2021-04-28 08:54:40 +0200

    .

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='templates')
@@ -50,3 +52,8 @@ def data():
             a = value
             result = pswdcheck(a)
             return render_template('data.html', result=result)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
